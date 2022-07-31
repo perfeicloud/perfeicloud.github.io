e43e117aaec95ebec2009bcdbdbf80bc39179ccb
binbr <binbr@163.com>
2022-07-31 10:09:35 +0800

    initialization.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,35 @@ import { Hero, Features, Cta } from './components'
 
 const App: React.FC = () => {
 
-  const [isDark, setDarkMode] = useState<boolean>(initIsDark())
+  const [isDark, setIsDark] = useState<boolean>(initIsDark() ?? false)
+
+  // component mounted
+  useEffect(() => {
+    // body loaded
+    document.body.classList.add('is-loaded', 'is-boxed')
+    // Reveal animations
+    document.body.classList.contains('has-animations') && ScrollReveal().reveal('.feature', {
+      duration: 600,
+      distance: '20px',
+      easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+      origin: 'right',
+      viewFactor: 0.2
+    })
+  }, [])
+
+  // isDark updated
+  useEffect(() => {
+    // set page dark mode
+    if (isDark) {
+      document.body.classList.add('lights-off')
+    } else {
+      document.body.classList.remove('lights-off')
+    }
+    // set cookie
+    const d = new Date()
+    d.setTime(d.getTime() + 24*3600*1000)
+    document.cookie = `isDark=${isDark?'1':'0'};expires=${d.toUTCString()}`
+  }, [isDark])
 
   function initIsDark() {
     // get cookie
@@ -17,36 +45,9 @@ const App: React.FC = () => {
     return (window.matchMedia('(prefers-color-scheme: dark)').matches)
   }
 
-  function changeIsDark(value: boolean) {
-    // set cookie
-    const d = new Date()
-    d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
-    document.cookie = `isDark=${value?'1':'0'};expires=${d.toUTCString()}`
-    setDarkMode(value)
-  }
-
-  useEffect(() => {
-    // body loaded
-    document.body.classList.add('is-loaded', 'is-boxed')
-    // set page dark mode
-    if (!isDark) {
-      document.body.classList.remove('lights-off')
-    } else {
-      document.body.classList.add('lights-off')
-    }
-    // Reveal animations
-    document.body.classList.contains('has-animations') && ScrollReveal().reveal('.feature', {
-      duration: 600,
-      distance: '20px',
-      easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-      origin: 'right',
-      viewFactor: 0.2
-    })
-  })
-
   return (
     <>
-      <Header isDark={isDark} handleIsDark={changeIsDark} />
+      <Header isDark={isDark} handleIsDark={setIsDark} />
       <Main>
         <Hero />
         <Features className='section'/>
